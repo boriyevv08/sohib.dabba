ada1e0ccd820a11b8505c805b32d704d9def7714
boriyevv08 <manoken999@gmail.com>
2022-10-10 00:49:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+let a= prompt("Sohibni dabbaligini isbotlash uchun Xa deb yozing");
+      if (a=="Xa"){
+            console.log("Sohib dabba")
+      document.write ("Xa to'g'ri, Sohib chista dabba");
+
+      }
+      else { console.log("Sohib baribr dabba")
+      document.write ("Ko'takansiz, Sohib baribr dabba");
+};
